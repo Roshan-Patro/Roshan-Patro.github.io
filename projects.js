d81a0693326014bb4ca4d8c67c3993921157d998
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2022-10-26 02:25:18 +0530

    progress added

---
+++ projects.js
@@ -0,0 +1,20 @@
+let brtlnSrcBtn = document.querySelector("#brightline_source_code_btn");
+brtlnSrcBtn.onclick = () =>{
+    window.open("https://github.com/Roshan-Patro/Brightline-Clone", '_blank');
+}
+
+let brtlnLiveBtn = document.querySelector("#brightline_live_btn");
+brtlnLiveBtn.onclick = () =>{
+    window.open("https://peppy-valkyrie-ddd5ab.netlify.app/", '_blank');
+}
+
+let zee5SrcBtn = document.querySelector("#zee5_source_code_btn");
+zee5SrcBtn.onclick = () =>{
+    window.open("https://github.com/Roshan-Patro/ZEE5-Clone", '_blank');
+}
+
+let zee5LiveBtn = document.querySelector("#zee5_live_btn");
+zee5LiveBtn.onclick = () =>{
+    window.open("https://zee5-clone-roshan.netlify.app//", '_blank');
+}
+
