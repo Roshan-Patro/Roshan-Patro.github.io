e9bba82e4b52b93c972024e341a5cd4c8d42f76c
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2023-04-29 22:43:28 +0530

    the whole new debugged code pushed

---
+++ js/home.js
@@ -0,0 +1,5 @@
+const resumeBtn = document.querySelector(".resumeButton");
+
+resumeBtn.onclick = ()=>{
+    window.open("https://drive.google.com/file/d/1e21ueYGnk5iwn88tFeywiy6knhU0KaUY/view?usp=sharing", '_blank');
+}
---
+++ js/navbar.js
@@ -0,0 +1,37 @@
+// Changing navbar's background upon scrolling
+let nav = document.querySelector('#nav-menu');
+
+ changenavBack();
+
+ window.addEventListener('scroll', changenavBack);
+ function changenavBack()
+ {
+     if (window.scrollY >= 25) {
+         nav.classList.add('active_nav');
+     } else {
+         nav.classList.remove('active_nav');
+     }
+ }
+
+//  Activating hamburger menu
+const body = document.querySelector("body");
+const navbar = document.querySelector("#nav-menu");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const resumeBtnOnNavbar = document.querySelector("#resumeBtnOnNavbar");
+
+resumeBtnOnNavbar.onclick = () =>{
+    window.open("https://drive.google.com/file/d/1e21ueYGnk5iwn88tFeywiy6knhU0KaUY/view?usp=sharing", '_blank');
+}
+
+menuBtn.onclick = ()=>{
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+}
+
+cancelBtn.onclick = ()=>{
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+}
